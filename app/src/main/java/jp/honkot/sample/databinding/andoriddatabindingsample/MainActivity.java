d45cbe5977aaae546dd3e06062c0cd66946772fc
Hiroki Honda <hon87kot12.xp@gmail.com>
2017-10-30 21:40:17 -0700

    Simple official source

--- app/src/main/java/jp/honkot/sample/databinding/andoriddatabindingsample/MainActivity.java
+++ app/src/main/java/jp/honkot/sample/databinding/andoriddatabindingsample/MainActivity.java
@@ -1,7 +1,10 @@
 package jp.honkot.sample.databinding.andoriddatabindingsample;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /** for listener */
+    public void onClickedButton(View view) {
+        final EditText tv = (EditText) findViewById(R.id.inputArea);
+        Toast.makeText(this, tv.getText().toString(), Toast.LENGTH_SHORT).show();
     }
 }
